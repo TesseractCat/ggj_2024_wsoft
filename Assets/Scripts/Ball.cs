c3497f77cfb0f43bdb6cead042d0b69052331641
Earlgrey99 <32375464+Earlgrey99@users.noreply.github.com>
2024-01-27 14:26:50 -0500

    Added hole colliders, OnTriggerEnter event for ball prefab

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,27 +7,69 @@ public class Ball : MonoBehaviour
     public string ball_id;
     public Material ball_material;
     public GameObject gameMngr;
+    public GameObject ball;
     
     //constructor
-    public Ball (string id_in, Material material_in)
+    /*public Ball (string id_in, Material material_in)
     {
         ball_id = id_in;
         ball_material = material_in;
-    }
+    }*/
 
     void Start()
     {
         gameObject.GetComponent<MeshRenderer>().material = ball_material;
+        gameMngr = GameObject.FindGameObjectWithTag("GameManager");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.GetComponent<Ball>().ball_id.Equals("death")) //destroy ball if collides with death ball
+        //TODO: decide if balls should disappear on collision with death ball
+        /*if (collision.gameObject.GetComponent<Ball>().ball_id.Equals("death")) //destroy ball if collides with death ball
         {
             Destroy(gameObject);
             return;
+        }*/
+
+        if ((collision.gameObject.GetComponent<Ball>().ball_id.Equals(gameMngr.GetComponent<GameMngr>().humor1)
+            || collision.gameObject.GetComponent<Ball>().ball_id.Equals(gameMngr.GetComponent<GameMngr>().humor2))
+            && (gameObject.GetComponent<Ball>().ball_id.Equals(gameMngr.GetComponent<GameMngr>().humor1)
+            || gameObject.GetComponent<Ball>().ball_id.Equals(gameMngr.GetComponent<GameMngr>().humor2))) { //if match is made
+
+            gameMngr.GetComponent<GameMngr>().ChangeCombos();   //change correct combination
         }
 
+        else    //if incorrect match is made
+        {
+            Vector3 pos = collision.gameObject.transform.position;
+            Destroy(collision.gameObject);
+            GameObject deathBall = Instantiate(ball, pos, Quaternion.identity);
+            deathBall.GetComponent<Ball>().ball_id = "death";
+            //TODO: load death ball material from assets
+            //deathBall.GetComponent<Ball>().ball_material = 
+            GameObject newBall = Instantiate(ball, new Vector3(0, 1.2f, 0), Quaternion.identity);
+            int newHumor = Random.Range(0, 4);
+            newBall.GetComponent<Ball>().ball_id = gameMngr.GetComponent<GameMngr>().humors[newHumor];
+            //TODO: load new ball material from assets
+            //newBall.GetComponent<Ball>().ball_material = 
+            Destroy(gameObject);
+            return;
+        }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Hole")) //not sure if we need this tbh, the only triggers in the scene are the holes
+        {
+            if (ball_id.Equals("death"))
+            {
+                //TODO: die!1!1! mwahaha
+            }
+            else
+            {
+                //TODO: decide how much score should decrease when opponent scores
+                gameMngr.GetComponent<GameMngr>().UpdateScore(10);
+            }
+        }
     }
 }
--- Assets/Scripts/GameMngr.cs
+++ Assets/Scripts/GameMngr.cs
@@ -4,46 +4,114 @@ using UnityEngine;
 
 public class GameMngr : MonoBehaviour
 {
-    public int combo;
+    // humors that are allowed to collide
+    public string humor1;
+    public string humor2;
+
+    public List<string> humors = new List<string>
+    {
+        "yellow", "black", "red", "blue"
+    };
 
     public List<GameObject> lights;
+
+    int player1score;
+    int player2score;
+
+    int turn; // turn = 0 when player1 turn, turn = 1 when player2 turn
+
+    bool turnTaken; // true = player has hit the ball and can no longer use the cue, false = player can use the cue
+
+    GameObject[] balls;
 
     void Start()
     {
         ChangeCombos();
+        player1score = 100;
+        player2score = 100;
+        turnTaken = false;
     }
 
     void Update()
     {
-        
+        CheckBalls(); //check if balls are still moving
+        if (turnTaken)
+        {
+            //TODO: disable cue mvmt, etc
+        }
+        else
+        {
+            //TODO: enable cue mvmt, etc
+        }
     }
 
-    void ChangeCombos()
+    public void ChangeCombos()
     {
-        int combos = Random.Range(0, 3);
-        switch (combos)
+        for (int i = 0; i < lights.Count; i++)
         {
-            // combo 0 = yellow + red, black + blue
-            case 0:
-                lights[1].SetActive(true);
-                lights[2].SetActive(false);
-                lights[3].SetActive(true);
-                lights[4].SetActive(false);
-                break;
-            // combo 1 = black + red, yellow + blue
-            case 1:
-                lights[1].SetActive(false);
-                lights[2].SetActive(true);
-                lights[3].SetActive(true);
-                lights[4].SetActive(false);
-                break;
-            // combo 2 = blue + red, yellow + black
-            case 2:
-                lights[1].SetActive(false);
-                lights[2].SetActive(false);
-                lights[3].SetActive(true);
-                lights[4].SetActive(true);
-                break;
+            lights[i].SetActive(false);
+        }
+
+        int combo1 = Random.Range(0, 4);
+
+        List<int> numsLeft = new List<int>();
+        for (int i = 0; i < 4; i++)
+        {
+            if (i != combo1)
+            {
+                numsLeft.Add(i);
+            }
+        }
+
+        int combo2 = numsLeft[Random.Range(0, 3)];
+
+        humor1 = humors[combo1];
+        humor2 = humors[combo2];
+
+        lights[combo1].SetActive(true);
+        lights[combo2].SetActive(true);
+    }
+
+    private void CheckBalls()
+    {
+        balls = GameObject.FindGameObjectsWithTag("Ball");
+        for (int i = 0; i < balls.Length; i++)
+        {
+            if (balls[i].GetComponent<Rigidbody>().velocity != new Vector3(0, 0, 0))    //if any of the balls in the scene are in motion then return
+            {
+                turnTaken = true;
+                return;
+            }
+        }
+        if (turnTaken)
+        {
+            turnTaken = false;  //next player has yet to hit a ball
+            if (turn == 0)  //turn transfers to opponent
+            {
+                turn = 1;
+            }
+            else if (turn == 1)
+            {
+                turn = 0;
+            }
+            StartRound();
+        }
+    }
+
+    public void StartRound()
+    {
+        //TODO: UI stuff
+    }
+
+    public void UpdateScore(int modifier)
+    {
+        if (turn == 0)
+        {
+            player2score -= modifier;   // if player1 scores decrease player2 score
+        }
+        else if (turn == 1)
+        {
+            player1score -= modifier;   // if player2 scores decrease player1 score
         }
     }
 }
